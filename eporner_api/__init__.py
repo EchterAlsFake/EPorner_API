ada6a4056393856d9515451303a5b3279127155e
Johannes Habel <EchterAlsFake@proton.me>
2026-07-19 23:59:16 +0200

    - added video to init

--- eporner_api/__init__.py
+++ eporner_api/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ["Client", "locals", "sorting", "errors", "consts", "DownloadConfigRAW"]
+__all__ = ["Client", "locals", "sorting", "errors", "consts", "DownloadConfigRAW", "Video"]
 
-from eporner_api.api import Client, DownloadConfigRAW
+from eporner_api.api import Client, DownloadConfigRAW, Video
 from eporner_api.modules import locals, sorting, errors, consts
